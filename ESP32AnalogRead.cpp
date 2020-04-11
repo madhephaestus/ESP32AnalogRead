758cbf4ce36fe93c9fd1bf5b7454d50c5fc92ace
Kevin Harrington <mad.hephaestus@gmail.com>
2020-04-11 11:22:16 -0400

    Adding calibrated read

--- ESP32AnalogRead.cpp
+++ ESP32AnalogRead.cpp
@@ -9,11 +9,26 @@
 #include "ESP32AnalogRead.h"
 
 
-
-ESP32AnalogRead::ESP32AnalogRead(){
-
+void ESP32AnalogRead::attach(int pin){
+	myPin=pin;
+	channel = (adc_channel_t)digitalPinToAnalogChannel(myPin);
 }
 
-void ESP32AnalogRead::attach(int pin){
+float ESP32AnalogRead::readVoltage(){
+	float mv= readMiliVolts();
 
+	return mv/1000.0;
+}
+uint32_t  ESP32AnalogRead::readMiliVolts(){
+	analogRead(myPin);
+    // Configure ADC
+    adc1_config_width(ADC_WIDTH_12Bit);
+    adc1_config_channel_atten(ADC1_CHANNEL_6, ADC_ATTEN_11db);
+
+    // Calculate ADC characteristics i.e. gain and offset factors
+    esp_adc_cal_get_characteristics(V_REF, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, &characteristics);
+    uint32_t voltage=0;
+    // Read ADC and obtain result in mV
+    esp_adc_cal_get_voltage(channel, &characteristics,&voltage);
+    return voltage-50;
 }
--- ESP32AnalogRead.h
+++ ESP32AnalogRead.h
@@ -1,8 +1,18 @@
 #pragma once
+#include <Arduino.h>
+#include "esp_adc_cal.h"
+#define V_REF 1100  // ADC reference voltage
+
 
 class ESP32AnalogRead {
+private:
+	int myPin=0;
+	 esp_adc_cal_characteristics_t characteristics;
+	 adc_channel_t channel ;
 public :
-	ESP32AnalogRead();
+	ESP32AnalogRead(){}
 	~ESP32AnalogRead(){}
 	void attach(int pin);
+	float readVoltage();
+	uint32_t readMiliVolts();
 };
